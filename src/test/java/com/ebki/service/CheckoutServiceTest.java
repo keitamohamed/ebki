5e3bb4f26ccb96547c7e596f3bef6163da1f0d5a
mohamedkeita <!2Mohamed>
2021-10-12 14:18:53 -0400

    Added code comments in CheckoutServiceTest class

--- src/test/java/com/ebki/service/CheckoutServiceTest.java
+++ src/test/java/com/ebki/service/CheckoutServiceTest.java
@@ -19,7 +19,7 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.never;
 
-class CarCheckoutServiceTest {
+class CheckoutServiceTest {
 
     @Mock
     private CheckOutRepo repo;
@@ -35,26 +35,31 @@ class CarCheckoutServiceTest {
 
     @Test
     void itShouldCheckOutCar() {
-        // Given
+        // Given...NEW CHECKOUT INFORMATION
         long id = 672635511L;
         Car car = new Car(id, "Nissan", "Maxima", "Open Top", 2004);
         CarCheckout checkout = new CarCheckout();
 
         checkout.setCheckoutID(6738161L);
         checkout.setCar(car);
-        // When
+
+        // When...CHECKOUT ID, IT SHOULD RETURN EMPTY OPTIONAL
         given(repo.findById(checkout.getCheckoutID()))
                 .willReturn(Optional.empty());
-        // Then
+
+        // Then...IT SHOULD SAVE CHECKOUT REQUEST
         carCheckoutService.checkOutCar(checkout);
+        //...IT SHOULD SAVE THE REQUEST AND CAPTURE IT
         then(repo).should().save(argumentCaptor.capture());
+        //...IT SHOULD RETURN THE CAPTURE VALUE
         CarCheckout capture = argumentCaptor.getValue();
+        //...IT SHOULD CHECK THAT THE CAPTURE VALUE IS EQUAL TO THE ORIGINAL REQUEST
         assertThat(capture).isEqualTo(checkout);
     }
 
     @Test
     void itShouldNotCheckOutCarWhenCarExists() {
-        // Given
+        // Given...NEW CHECKOUT INFORMATION
         long id = 672635511L;
         Car car = new Car(id, "Nissan", "Maxima", "Open Top", 2004);
         CarCheckout checkout = new CarCheckout();
@@ -62,18 +67,19 @@ class CarCheckoutServiceTest {
         checkout.setCheckoutID(6728121L);
         checkout.setCar(car);
 
+        //...Given CHECKOUT ID, IT SHOULD RETURN EMPTY OPTIONAL
         given(repo.findById(checkout.getCheckoutID()))
                 .willReturn(Optional.of(checkout));
 
-        // When
+        // When...IT SHOULD NOT SAVE THE CHECKOUT REQUEST SINCE THE CHECKOUT ALREADY EXIST
         carCheckoutService.checkOutCar(checkout);
-        // Then
+        // Then...IT SHOULD CHECK THAT THE CHECKOUT REQUEST WAS NOT SAVE
         then(repo).should(never()).save(any());
     }
 
     @Test
     void itShouldThrowExceptionWhenCheckIDExists() {
-        // Given
+        // Given...NEW CHECKOUT INFORMATION
         long id = 672635511L;
         Car car = new Car(6271718L, "Nissan", "Maxima", "Open Top", 2004);
         Car carTwo = new Car(6271718L, "Nissan", "37Z", "Open Top", 2004);
@@ -86,22 +92,24 @@ class CarCheckoutServiceTest {
         checkout.setCar(car);
         checkoutTwo.setCar(carTwo);
 
+        //...IT SHOULD RETURN CHECKOUT WITH GIVEN ID THAT IS SAME AS CHECKOUT TWO
         given(repo.findById(checkout.getCheckoutID()))
                 .willReturn(Optional.of(checkoutTwo));
 
         // When
-        // Then
+        // Then ...IT SHOULD THROW ILLEGAL STATE EXCEPTION SINCE GIVEN ID ALREADY EXISTS
         assertThatThrownBy(() -> carCheckoutService.checkOutCar(checkout))
                 .isInstanceOf(IllegalStateException.class)
                         .hasMessageContaining(String.format(
                                 "Car [%s] with Vin number [%s] is already checkout ",
                                 car.getBrand(), car.getCarVinNumber()));
+        //...IT SHOULD CHECK THAT THE REQUEST WAS NOT SAVE
         then(repo).should(never()).save(any(CarCheckout.class));
     }
 
     @Test
     void itShouldCheckOutCarWhenCheckOutIDIsNull() {
-        // Given
+        // Given...NEW CHECKOUT INFORMATION
         long id = 672635511L;
         Car car = new Car(id, "Nissan", "Maxima", "Open Top", 2004);
 
@@ -109,18 +117,21 @@ class CarCheckoutServiceTest {
         checkout.setCar(car);
         checkout.setCheckoutID(null);
 
+        //...GIVEN THE CAR VIN NUMBER, IT SHOULD RETURN AN EMPTY OPTIONAL OF CHECKOUT
         given(repo.findCarCheckoutByCar_CarVinNumber(car.getCarVinNumber()))
                 .willReturn(Optional.empty());
-        // When
+        // When...IT SHOULD SAVE THE CHECKOUT REQUEST
         carCheckoutService.checkOutCar(checkout);
 
-        // Then
+        // Then...IT SAVE THE REQUEST AND CAPTURE THE REQUEST
         then(repo).should().save(argumentCaptor.capture());
-        CarCheckout checkout1 = argumentCaptor.getValue();
-        assertThat(checkout1)
+        //...IT SHOULD RETURN THE CAPTURE VALUE
+        CarCheckout captureValue = argumentCaptor.getValue();
+        assertThat(captureValue)
                 .usingRecursiveComparison()
                 .ignoringFields("name", "checkoutID")
                 .isEqualTo(checkout);
-        assertThat(checkout1.getCheckoutID()).isNotNull();
+        //...IT SHOULD CHECK THAT CAPTURE VALUE IS NOT NULL
+        assertThat(captureValue.getCheckoutID()).isNotNull();
     }
 }
