eb00d2ca732498a3a02754a939f79f252a5fea53
mohamedkeita <!2Mohamed>
2021-10-31 02:26:29 -0400

    Can add driver using frontend app.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './component/style/at_responsive.css'
+
+import {DynamicRoute} from "./route/DynamicRoute";
+
+function App() {
+  return (
+    <DynamicRoute/>
+  );
+}
+
+export default App;
